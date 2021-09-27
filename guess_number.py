6abd00542f16b4337de49e1b2667373c8a45b673
Joe Xie <ugb14129@gmail.com>
2021-09-26 23:59:20 -0500

    V2

--- guess_number.py
+++ guess_number.py
@@ -1,13 +1,16 @@
 import random
 r = random.randint(1 , 100)
-
+count = 0
 while True:
+	count = count + 1
 	num = int(input('Please guess a number: '))
 	if num == r:
 		print('Matched')
+		print('This is your' , count , 'time')
 		break
 	elif num > r:
 		print('Guess larger than the number')
 	elif num < r:
 		print('Guess less than the number')
+	print('This is your' , count , 'time')
 
